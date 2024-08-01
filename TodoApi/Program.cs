d725b57532158e07730a14eaf650f8fa3de48708
Altaf Mehmood <altaf.mehmood@gmail.com>
2024-07-31 23:20:20 -0500

    Introduce password in program.cs

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -7,6 +7,11 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+var userId = "john";
+var password = "doe";
+
+Console.WriteLine($"{userId} - {password}");
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
