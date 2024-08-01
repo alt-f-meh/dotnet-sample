97ee487d00e7f5dc7949998afd2bdecaa1fdec11
Altaf Mehmood <altaf.mehmood@gmail.com>
2024-07-31 23:16:34 -0500

    Docker execute

--- TodoApiTest/UnitTest1.cs
+++ TodoApiTest/UnitTest1.cs
@@ -9,10 +9,14 @@ public class UnitTest1
     [TestMethod]
     public void TestMethod1()
     {
+        // Arrange
         var logger = NSubstitute.Substitute.For<ILogger<WeatherForecastController>>();
         var controller = new WeatherForecastController(logger);
 
+        // Act
         var items = controller.Get();
+
+        // Assert
         Assert.IsNotNull(items);
         Assert.IsTrue(items?.Count() > 0);
     }
